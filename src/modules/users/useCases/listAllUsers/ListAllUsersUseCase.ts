782218c077ae87aec8e5639bc5a316482866b935
Allex Valério Costa <allex.developer@gmail.com>
2021-04-15 21:14:46 -0300

    Quarto Desafio Ignite NodeJS

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User don't Exists!");
+    } else if (user.admin != true) {
+      throw new Error("User is not a Admin!");
+    }
+    
+    return this.usersRepository.list();
   }
 }
 
